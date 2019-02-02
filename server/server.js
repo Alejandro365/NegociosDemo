a7d4bad53b654523a0f4bfe8e12be65033408d9f
César Mejía <checha365@gmail.com>
2019-02-02 15:06:08 -0600

    Se creo conexión a la base de datos 020219

--- server/server.js
+++ server/server.js
@@ -1,15 +1,26 @@
 var express = require('express');
+var mysql = require ('mysql');
 var server = express();
+
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'negocios',
+    database : 'negocios'
+  });
+
+  console.log("Conectado a la DB");
 
 server.get('/',function(req, res){
 
     res.send("HOLA MUNDO"); 
-
 });
 
 server.get('/login', function (req, res){
-
+    
     res.send("HOLA DESDE LOGIN");
 });
 
 server.listen(3000);
+
+//Toda invocacióna una función es asincrona
